1b9338b95a15b7c9788b10fd8e24e38ba42047d7
Juan Jose Gallego <jjosegallegomesa@gmail.com>
2025-12-05 13:40:19 -0500

    ok

--- src/main/java/org/example/controladores/EstudianteControlador.java
+++ src/main/java/org/example/controladores/EstudianteControlador.java
@@ -13,6 +13,27 @@ import java.util.List;
 public class EstudianteControlador {
 
 
+    //Ojo pues muchachos, aca vamos a programar el CRUD
+
+    public void usuarioPOST(Estudiante estudiante){
+        String consultaSQL = "INSERT INTO estudiantes(nombre, carrera) VALUES(?,?)";
+
+        try (Connection conexion = new ConexionBD().conectar();
+             PreparedStatement parceroSQL = conexion.prepareStatement(consultaSQL)) {
+
+
+            parceroSQL.setString(1, estudiante.getNombre());
+            parceroSQL.setString(2, estudiante.getCarrera());
+            parceroSQL.executeUpdate();
+
+            System.out.println("😊 ¡Éxito guardando los datos!");
+
+        } catch(Exception error) {
+            System.out.println("❌ Fallamos guardando: " + error.getMessage());
+        }
+    }
+
+
 
 
 }
